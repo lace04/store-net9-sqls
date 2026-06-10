ad149e334cf50bcb495e86cfefb32a2b3b933a61
lace04 <luisace04@gmail.com>
2026-06-10 14:49:18 -0500

    Agregar un interruptor aria-expanded para el botón del menú móvil

--- Store/wwwroot/js/site.js
+++ Store/wwwroot/js/site.js
@@ -3,6 +3,8 @@
   var menu = document.getElementById('mobile-menu');
   if (toggle && menu) {
     toggle.addEventListener('click', function () {
+      var expanded = toggle.getAttribute('aria-expanded') === 'true';
+      toggle.setAttribute('aria-expanded', !expanded);
       menu.classList.toggle('hidden');
     });
   }
