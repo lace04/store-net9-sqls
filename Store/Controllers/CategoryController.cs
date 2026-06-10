bb31320c584bda7e7b655b68cd7931f65a089ecc
lace04 <luisace04@gmail.com>
2026-06-10 13:51:27 -0500

    Añadida funcionalidad de gestión de categorías
    Se realizaron los siguientes cambios:
    - Reorganización y limpieza del archivo `_Layout.cshtml`, incluyendo un nuevo enlace en la barra de navegación para "Categorías".
    - Modificación de `_ViewImports.cshtml` para incluir el espacio de nombres `Store.Entitites`.
    - Creación del controlador `CategoryController` para manejar la lógica de categorías.
    - Adición de la vista `Index.cshtml` para mostrar una tabla con la lista de categorías y acciones asociadas.
    - Integración completa de la funcionalidad de categorías en la aplicación.

---
+++ Store/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Context;
+
+namespace Store.Controllers
+{
+  public class CategoryController(AppDbContext _dbContext) : Controller
+  {
+    public IActionResult Index()
+    {
+
+      // Get list of categories from database
+      var categories = _dbContext.Categories.ToList();
+
+      return View(categories);
+    }
+  }
+}
